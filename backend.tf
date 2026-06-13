e4c865199ed5a20d7996e3ddfdf309ae2d5baeb7
Fir3eye <93431222+Fir3eye@users.noreply.github.com>
2026-06-13 18:36:13 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "sudheer-terraform-state-bucket"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
